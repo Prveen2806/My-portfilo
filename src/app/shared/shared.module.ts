c3da749707915b7a6211c8686480dfb5e1c20b5e
Rajesh <rajeshm@gramosoft.in>
2025-01-02 19:43:32 +0530

    initial commit

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    NgSelectModule, FormsModule
+  ]
+})
+export class SharedModule { }
